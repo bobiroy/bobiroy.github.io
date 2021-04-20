226f43f3646ebfae1200a83e6617fdaf0dbd1575
bobiroy <fredrich.ebenezer@gmail.com>
2021-04-20 09:10:24 +0700

    deploy

--- scripts.js
+++ scripts.js
@@ -16,5 +16,5 @@ app.get('/', function(req, res, next) {
                 res.json(response.content);
             });
 });
-
-app.listen(3000);
+app.listen(process.env.PORT || 5000, 
+	() => console.log("Server is running..."));
